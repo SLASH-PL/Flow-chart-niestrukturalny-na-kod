d0ea063b25c5f62cdcef28088d0a7544bc8aa2f6
Hubert Haładyna <hubert.haladyna@microsoft.wsei.edu.pl>
2023-11-13 18:36:39 +0100

    Dodaj pliki projektów.

---
+++ Flow-chart niestrukturalny na kod/with-goto.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.Design;
+
+string input = Console.ReadLine();
+string[] liczby = input.Split(' ');
+int x = int.Parse(liczby[0]);
+int y = int.Parse(liczby[1]);
+int z = int.Parse(liczby[2]);
+
+Start:
+if(x > 0)
+{
+    if(y > 0)
+    {
+        Console.Write("C");
+        x = x - 1;
+        y = y - 1;
+        goto Start;
+    }
+    else if(y <= 0)
+    {
+        Console.Write("D");
+        if(z > 0) 
+        {
+            Console.WriteLine();
+        }
+        else if(z <= 0) 
+        {
+            Console.Write("G");
+            Console.WriteLine();
+        }
+    }
+}
+else if(x <= 0)
+{
+    Console.Write("E");
+    Console.Write("G");
+    Console.WriteLine();
+}
---
+++ with-for/with-for.cs
@@ -0,0 +1,35 @@
+﻿string input = Console.ReadLine();
+string[] liczby = input.Split(' ');
+int x = int.Parse(liczby[0]);
+int y = int.Parse(liczby[1]);
+int z = int.Parse(liczby[2]);
+
+for(; x > 0 && y > 0; )
+{
+    Console.Write("C");
+    y = y - 1;
+    x = x - 1;
+}
+
+if (x > 0 && y <= 0)
+{
+    Console.Write("D");
+    if (z > 0)
+    {
+        Console.WriteLine();
+    }
+    else if (z <= 0)
+    {
+        Console.Write("G");
+        Console.WriteLine();
+    }
+}
+else if (x <= 0)
+{
+    Console.Write("E");
+    Console.Write("G");
+    Console.WriteLine();
+}
+
+
+//for( int a = 0; x > 0 && y > 0; a++)
